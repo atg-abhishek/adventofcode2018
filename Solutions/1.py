67673e1d9ec8a694816fd10264f2d0b7d457c553
Abhishek Gupta <abhisg@microsoft.com>
2018-12-10 10:26:55 -0500

    solved part 2 day 1

--- Solutions/1.py
+++ Solutions/1.py
@@ -1,8 +1,16 @@
+from itertools import cycle
 lines = []
 with open('../Data/Day1.txt') as f:
     lines = f.readlines()
-total = 0
-for line in lines:
+
+def part1():
+    total = 0
+    for line in lines:
+        num=process_input(line)
+        total+=num
+    print(total)
+
+def process_input(line):
     str_frequency = line.rstrip()
     sign = str_frequency[0]
     num = 0 
@@ -10,5 +18,20 @@ for line in lines:
         num = 0-int(str_frequency[1:])
     else:
         num = int(str_frequency[1:])
-    total+=num
-print(total)
+    return num
+
+def part2():
+    pool = cycle(lines)
+    total = 0
+    s = set()
+    s.add(total)
+    for item in pool:
+        num = process_input(item)
+        total += num
+        if total in s:
+            print("Repetition found at ", total)
+            break
+        else:
+            s.add(total)
+
+part2()
