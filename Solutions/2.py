7f52e95e4fe71c61af359b6c711f8babaf42e726
Abhishek Gupta <abhisg@microsoft.com>
2018-12-10 14:37:50 -0500

    solved part 2 day 2

--- Solutions/2.py
+++ Solutions/2.py
@@ -1,4 +1,5 @@
 from collections import Counter
+from copy import deepcopy
 lines = []
 with open("../Data/Day2.txt") as f:
     lines = f.readlines()
@@ -25,4 +26,38 @@ def part1():
             threes += 1
     return twos*threes
 
-print(part1())
+def compare_two_strings(s1, s2):
+    ok = False
+    char_of_diff = ''
+    for c1, c2 in zip(s1, s2):
+        if c1 != c2:
+            if ok:
+                return (c1, False)
+            else:
+                char_of_diff = c1
+                ok = True
+
+    return (char_of_diff, ok)
+
+def part2():
+    lines_copy = deepcopy(lines)
+    lines_copy.sort()
+    chars = set()
+    for idx, line in enumerate(lines_copy[:-1]):
+        for other_line in lines_copy[idx+1:]:
+            res = compare_two_strings(line.rstrip(), other_line.rstrip())
+            if res[1]:
+                print("this was hit")
+                chars = set(line.rstrip()) - set(res[0])
+                print(line)
+                print(res[0])
+                break
+
+
+    
+
+ 
+
+
+# print(compare_two_strings("abcde", "axcye"))
+print(part2())
